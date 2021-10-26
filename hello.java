9534c8090da0eaa8b96559343266bf2f4a32a5bb
dana-sun <ds5613@nyu.edu>
2021-10-26 17:06:15 -0400

    You can now add allergies

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+import java.util.ArrayList;
 
 
 public class hello {
@@ -8,8 +9,23 @@ public class hello {
         System.out.println("Welcome to Weet! Please enter your name");
         String username = obj.nextLine();
         System.out.println("Welcome, " + username + "!");
-        obj.close();
+        System.out.println("Please enter any food allergies you may have one by one. In order to stop, enter 'X'.");
+        String allergy = "";
+        ArrayList<String> allergies = new ArrayList<String>();
+        while (true) {
+            if (("X").equals(allergy)) {
+                break;
+            }
+            else {
+                allergy = obj.nextLine();
+                allergies.add(allergy);
+            }
+        }
+        //obj.close();
 
+        for (int i = 0; i < allergies.size(); i++) {
+            System.out.println(allergies.get(i));
+        }
     }
 }
 
