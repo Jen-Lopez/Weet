970f9c7e565477c301626b9aeec9833a26858719
Oliver Wang <owang00@gmail.com>
2021-11-09 14:33:59 -0500

    Add files via upload

---
+++ src/main/java/com/example/Weet/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.Weet;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    public User findByFirstName(String firstName);
+    public List<User> findByLastName(String lastName);
+    public List<User> findByusername(String username);
+
+}
---
+++ src/test/java/com/example/Weet/WeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Weet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
