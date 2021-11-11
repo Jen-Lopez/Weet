6c8f8eb58fb6137b5943acb73fc807ae82879501
Jen-Lopez <jennlolopz@gmail.com>
2021-11-11 14:45:59 -0500

    linked restaurant details route

--- demo/src/main/java/io/weet/demo/controllers/RestaurantDetailsController.java
+++ demo/src/main/java/io/weet/demo/controllers/RestaurantDetailsController.java
@@ -1,11 +1,19 @@
 package io.weet.demo.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RestaurantDetailsController {
     @GetMapping("/restaurant")
-    public String resolveRestaurantDetails() {
+    // public String fetchRestDetails() {
+    //     return "restaurantProfile";
+    // }
+
+    public String fetchRestDetails(@RequestParam("id") String restId, Model model) {
+        model.addAttribute("restaurantID", restId);
         return "restaurantProfile";
     }
 }
--- demo/src/main/resources/static/js/index.js
+++ demo/src/main/resources/static/js/index.js
@@ -1,11 +1,4 @@
-const getRestDetails = (restID) => {
-    // do get request to '/details' endpoint. Return the data.
-    console.log("MADE A REQUEST TO GET " + restID);
-    getRequest("GET", "/restaurant");
-}
-
-function getRequest(method, endpoint) {
-    let req = new XMLHttpRequest();
-    req.open(method, endpoint)
-    req.send();
+const redirectToDetails = (id) => {
+    console.log("THE ID IS " + id);
+    window.location.href=`/restaurant?id=${id}`
 }
