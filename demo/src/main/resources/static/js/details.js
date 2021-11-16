5fa0bd4ef724b7e044694f1a6390b1a051013003
Jen-Lopez <jennlolopz@gmail.com>
2021-11-16 10:40:35 -0500

    add popover

--- demo/src/main/resources/static/js/details.js
+++ demo/src/main/resources/static/js/details.js
@@ -6,4 +6,20 @@ const showMenu = () => {
     menuList.classList.toggle("showMenu");
 }
 
+$(function () {
+    $('[data-toggle="popover"]').popover({ 
+        trigger: 'focus',
+        content: function() {
+            let description = $(this)[0].dataset.description;
+            if (description) {
+                return $(this)[0].dataset.description;
+            }
+            return "Description not available."
+        },
+        title: function() {
+            return $(this)[0].dataset.title;
+        },
+    })
+})
+
 menuButton.addEventListener("click", showMenu);
