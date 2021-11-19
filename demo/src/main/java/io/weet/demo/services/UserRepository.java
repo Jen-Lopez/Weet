b1d4673a1f738a66ace3defebfb798806a0f6ecd
Siddharth Datar <srd404@nyu.edu>
2021-11-19 15:33:20 -0500

    installed dependencies and worked on UserServices

--- demo/src/main/java/io/weet/demo/services/UserRepository.java
+++ demo/src/main/java/io/weet/demo/services/UserRepository.java
@@ -2,6 +2,7 @@ package io.weet.demo.services;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import io.weet.demo.models.Allergen;
 import io.weet.demo.models.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
@@ -9,5 +10,6 @@ public interface UserRepository extends MongoRepository<User, String> {
     public User findByFirstName(String firstName);
     public List<User> findByLastName(String lastName);
     public List<User> findByusername(String username);
+    public void addAllergies(User user, Allergen allergen);
 
 }
--- demo/src/main/java/io/weet/demo/services/UserService.java
+++ demo/src/main/java/io/weet/demo/services/UserService.java
@@ -7,7 +7,7 @@ import io.weet.demo.models.User;
 
 public interface UserService {
     User saveUser(User user);
-    void addAllergies(Allergen allergen);
+    void addAllergies(User user, Allergen allergen);
     User getUser(String username); //fetches user from Database based on name
     List<User>getUsers(); //fetches all users; may not need
 }
