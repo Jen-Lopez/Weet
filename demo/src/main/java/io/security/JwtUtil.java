f712fcd4d689d6d5a9e4b3f58db2c644844e59c1
Siddharth Datar <srd404@nyu.edu>
2021-11-20 10:32:19 -0500

    validating tokens

--- demo/src/main/java/io/security/JwtUtil.java
+++ demo/src/main/java/io/security/JwtUtil.java
@@ -56,6 +56,12 @@ public class JwtUtil {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*10)).signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
+    
+    //validate tokens
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) &&!isTokenExpired(token));
+    }
 
 
 
