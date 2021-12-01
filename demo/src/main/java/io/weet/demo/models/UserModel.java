8c657493875196ea1cd50dcdd366f00428f7a3b1
Siddharth Datar <srd404@nyu.edu>
2021-12-01 10:01:40 -0500

    Few more methods for UserModal for setup

--- demo/src/main/java/io/weet/demo/models/UserModel.java
+++ demo/src/main/java/io/weet/demo/models/UserModel.java
@@ -26,9 +26,18 @@ public class UserModel {
         return name;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
+
     public String getEmail() {
         return email;
-    }  
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
 
     public String getPassword() {
         return password;
